1a5efaf53cd5a8b5a8c820d5f810bf7e82db55c0
sannidhi82 <sannidhibhat34@gmail.com>
2024-08-02 18:25:05 +0530

    latest version

--- predictor/views.py
+++ predictor/views.py
@@ -1,77 +1,51 @@
 # views.py
 import os
 import numpy as np
-import matplotlib.pyplot as plt
 from django.shortcuts import render
 from django.conf import settings
 from .forms import ImageUploadForm
-from tensorflow.keras import models
-from tensorflow.keras.preprocessing import image
 from predictor.models import UploadedModel
 from django.shortcuts import render, redirect
+import requests
+import json
+import requests
+from gradio_client import Client, handle_file
 
+GRADIO_API_URL = "https://sannidhi82-fundus-detection.hf.space/predict"
 
-def get_vgg_modal():
-    # Load the trained model
-    try:
-        vgg_modal = models.load_model(os.path.join(settings.BASE_DIR, 'static', 'modal', 'mymodal.h5'))
-        return vgg_modal
-    except Exception as e:
-        return None
-
-# Function to load and preprocess the image
-def load_and_preprocess_image(img_path):
-    img = image.load_img(img_path, target_size=(224, 224))
-    img_array = image.img_to_array(img)
-    img_array = np.expand_dims(img_array, axis=0)
-    img_array = img_array / 255.0
-    return img_array
-
-# View for handling image upload
 def upload_image(request):
     if request.method == 'POST':
         form = ImageUploadForm(request.POST, request.FILES)
         if form.is_valid():
             uploaded_image = request.FILES['image']
-            img_name = uploaded_image.name
-            img_path = os.path.join(settings.MEDIA_ROOT, img_name)
+            img_path = os.path.join(settings.MEDIA_ROOT, uploaded_image.name)
             
             # Save the uploaded image
             os.makedirs(settings.MEDIA_ROOT, exist_ok=True)
-
             with open(img_path, 'wb+') as destination:
                 for chunk in uploaded_image.chunks():
                     destination.write(chunk)
 
-            # Make prediction
-            img_array = load_and_preprocess_image(img_path)
-            predictions = get_vgg_modal().predict(img_array)
-            predicted_class = np.argmax(predictions, axis=1)[0]
-
-            # Define the label map
-            label_map = {0: 'Glaucoma', 1: 'Cataract', 2: 'Diabetic Retinopathy', 3: 'Normal'}
-
-            # Check if predicted_class is valid
-            if predicted_class in label_map:
-                predicted_label = label_map[predicted_class]
-            else:
-                predicted_label = "Unknown disease detected. Please consult a specialist."
-
+            try:
+                # Send image to the Gradio API for prediction
+                client = Client("sannidhi82/fundus-detection")
+                result = client.predict(
+                    uploaded_image=handle_file(img_path),
+                    api_name="/predict"
+                )
+            except Exception as e:
+                print(f"Error during API call: {e}")
 
             # Create the URL for the uploaded image
-            image_url = os.path.join(settings.MEDIA_URL, img_name)
+            image_url = os.path.join(settings.MEDIA_URL, uploaded_image.name)
 
             # Return the prediction result to the template
-            return render(request, 'result.html', {'predicted_label': predicted_label, 'image_path': image_url})
-
+            return render(request, 'result.html', {'predicted_label': result, 'image_path': image_url})
         else:
             print(form.errors)
-
     else:
         form = ImageUploadForm()
     return render(request, 'upload.html', {'form': form})
-
-
 
 def upload_model(request):
     if request.method == 'POST':
